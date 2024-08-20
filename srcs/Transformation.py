0a3547d7d96fa6d4409e030d8437a02871f8674b
bbordere <bastien.bordereau@gmail.com>
2024-08-20 23:20:17 +0200

    add roi filter + wip landmarks

--- srcs/Transformation.py
+++ srcs/Transformation.py
@@ -6,28 +6,70 @@ import sys
 import os
 from cv2.typing import MatLike
 
+pcv.params.line_thickness = 2
+pcv.params.dpi = 100
 
-def analyse_pcv(img: MatLike) -> MatLike:
-    gray_image = pcv.rgb2gray_hsv(rgb_img=img, channel="s")
 
-    bin_mask = pcv.threshold.binary(
-        gray_img=gray_image, threshold=100, object_type="light"
-    )
+def analyze_pcv(img: MatLike) -> MatLike:
+    gray_image = pcv.rgb2gray_lab(rgb_img=img, channel="a")
 
+    bin_mask = pcv.threshold.otsu(gray_img=gray_image, object_type="dark")
     roi = pcv.roi.rectangle(img=img, x=0, y=0, h=255, w=255)
 
     mask = pcv.roi.filter(mask=bin_mask, roi=roi, roi_type="partial")
 
-    shape_img = pcv.analyze.size(img=img, labeled_mask=mask, n_labels=1)
+    shape_img = pcv.analyze.size(img=img, labeled_mask=mask)
     pcv.plot_image(shape_img)
 
 
+import numpy as np
+
+
 def roi_pcv(img: MatLike) -> MatLike:
-    gray_image = pcv.rgb2gray_hsv(rgb_img=img, channel="s")
-    bin_img = pcv.invert(
-        pcv.threshold.binary(gray_img=gray_image, threshold=100, object_type="light")
+    cs = pcv.visualize.colorspaces(rgb_img=img, original_img=False)
+
+    a = pcv.rgb2gray_lab(rgb_img=img, channel="a")
+
+    bin_mask = pcv.threshold.triangle(gray_img=a, object_type="dark")
+
+    roi = pcv.roi.rectangle(img=img, x=0, y=0, h=256, w=256)
+    mask = pcv.roi.filter(mask=bin_mask, roi=roi, roi_type="partial")
+    colorized_mask = pcv.visualize.colorize_masks(masks=[mask], colors=["green"])
+    merged_image = pcv.visualize.overlay_two_imgs(colorized_mask, img, alpha=0.4)
+    cv2.rectangle(merged_image, (3, 3), (253, 253), (255, 0, 0), 3)
+    pcv.plot_image(merged_image)
+    return merged_image
+
+
+def landmarks_pcv(img: MatLike) -> MatLike:
+    a = pcv.rgb2gray_lab(rgb_img=img, channel="a")
+
+    bin_img = pcv.threshold.otsu(gray_img=a, object_type="dark")
+
+    homolog_pts, start_pts, stop_pts, ptvals, chain, max_dist = pcv.homology.acute(
+        img=img, mask=bin_img, win=13, threshold=90
     )
-    pcv.plot_image(bin_img)
+    res = img.copy()
+    third = len(homolog_pts) // 3
+    cv2.drawContours(res, homolog_pts[:third], -1, (255, 0, 0), 3)
+    cv2.drawContours(res, homolog_pts[third : third * 2], -1, (0, 255, 0), 3)
+    cv2.drawContours(res, homolog_pts[third * 2 :], -1, (0, 0, 255), 3)
+    print(start_pts, stop_pts, ptvals, chain, max_dist)
+    pcv.plot_image(res)
+
+
+def color_hist_pcv(img: MatLike) -> MatLike:
+    a = pcv.rgb2gray_lab(rgb_img=img, channel="a")
+
+    bin_img = pcv.threshold.otsu(gray_img=a, object_type="dark")
+    color_histogram = pcv.analyze.color(
+        rgb_img=img,
+        labeled_mask=bin_img,
+        colorspaces="all",
+        label="default",
+    )
+    pcv.plot_image(color_histogram)
+    cv2.imshow()
 
 
 def blur_pcv(img: MatLike) -> MatLike:
@@ -62,7 +104,11 @@ def apply_filter(path: str, filter: str) -> None:
         case "roi":
             roi_pcv(img)
         case "analyze":
-            analyse_pcv(img)
+            analyze_pcv(img)
+        case "landmarks":
+            landmarks_pcv(img)
+        case "intensity":
+            color_hist_pcv(img)
 
 
 def main() -> None:
